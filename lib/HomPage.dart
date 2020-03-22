79947728f42813e9ce6b6cf531a3f7b6e106dae9
Seung Tack Jo <seungtackjo@Seungs-MacBook-Pro.local>
2020-03-22 22:51:33 +0900

    first commit planetsApp with appBar

---
+++ lib/HomPage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'treva',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w600,
+            fontSize: 36.0
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Text('test'),
+          //HomePageBody(),
+        ],
+      ),
+    );
+  }
+}
